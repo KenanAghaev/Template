5119c5e82f43c5c1c4219ef0ffc73ec8128c520f
KenanAghaev <161588626+KenanAghaev@users.noreply.github.com>
2024-03-26 14:35:44 +0400

    Add files via upload

---
+++ app task/app.js
@@ -0,0 +1,45 @@
+//1.İstifadəçinin daxil etdiyi adın neçə simvoldan ibarət olduğunu tapın.
+
+// let ad=prompt("adinizi daxil edin");
+// console.log(ad.length);
+
+//2.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvol varmı ?.
+
+// let soz=prompt("sozu daxil edin:");
+// let herf=prompt("herfi daxil edin:");
+
+// let netice=soz.includes(herf);
+// console.log(netice);
+
+//3.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ?.
+
+// let soz=prompt("sozu daxil edin:");
+// let herf=prompt("herfi daxil edin:");
+
+// let ikinciherf=soz.charAt(1);
+// let yoxla=(ikinciherf===herf);
+// console.log(yoxla);
+
+//4.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla bitirmi ?.
+
+// let soz=prompt("sozu daxil edin:");
+// let herf=prompt("herfi daxil edin:");
+
+// let sonherf=soz.charAt(soz.length-1);
+// let yoxla=(sonherf===herf);
+// console.log(yoxla);
+
+//5.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvolun index`ini tapın.
+
+// let soz=prompt("sozu daxil edin:");
+// let herf=prompt("herfi daxil edin:");
+
+// let index=soz.indexOf(herf);
+// console.log(index);
+
+//6.Soz boyuk herflidirmi?
+
+// let soz=prompt("Sozu daxil edin:");
+// let yoxlama= soz === soz.toUpperCase();
+
+// console.log(yoxlama);
