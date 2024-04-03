e5ee3c77dea3374e675b50f09781e3a0ad438be6
KenanAghaev <161588626+KenanAghaev@users.noreply.github.com>
2024-04-03 14:01:53 +0400

    Add files via upload

---
+++ js task 5/app.js
@@ -0,0 +1,68 @@
+// let products = [
+//   {
+//     id: 1,
+//     name: "Sony 1000XM5",
+//     category: "Headphones",
+//     price: 7444,
+//   },
+//   {
+//     id: 2,
+//     name: "Apple Airpods Max",
+//     category: "Headphones",
+//     price: 3644,
+//   },
+//   {
+//     id: 3,
+//     name: "Samsung Galaxy S22",
+//     category: "Smartphones",
+//     price: 7458,
+//   },
+//   {
+//     id: 4,
+//     name: "Apple 13 Pro",
+//     category: "Smartphones",
+//     price: 5458,
+//   },
+// ];
+
+// products.sort((a,b)=>a.price-b.price);
+
+// // console.log(products);
+
+// products.forEach(function(product) {
+//     console.log("Mehsul:", product.name, "Qiymeti:", product.price);
+// });
+
+// 2ci sual
+
+// let a = Math.floor(Math.random()*100);
+// let b = Math.floor(Math.random()*100);
+// let c = Math.floor(Math.random()*100);
+// let d = Math.floor(Math.random()*100);
+// let e = Math.floor(Math.random()*100);
+// let array = [a,b,c,d,e];
+// let cem = 0;
+
+// for(let i = 0; i< array.length;i++){
+//     cem+=array[i];
+// }
+// console.log(array);
+// console.log(cem);
+
+
+// let array = []
+
+// for (let i = 0; i < 5; i++){
+
+//     array.push(Math.floor(Math.random()*1000))
+// }
+
+// console.log(array);
+
+// let toplam = 0;
+
+// for (i = 0; i<array.length; i++){
+//     toplam += array[i]
+// }
+
+// console.log("Cem:" + toplam);
